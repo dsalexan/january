cc7a1c38db9b0d3bff0014c47d6c717dc5d71f94
dsalexan <danilo.salexan@gmail.com>
2020-01-17 17:20:06 -0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    camelcase: 'off',
+    'max-len': [
+      'warn',
+      {
+        code: 130
+      }
+    ],
+    'prefer-destructuring': 'error',
+    'radix': 'off',
+    'consistent-return': 'error',
+    'vue/name-property-casing': 'off',
+    'import/no-named-as-default-member': 'off',
+    'consistent-return': 'off',
+    'prefer-destructuring': 'warn'
+  }
+}
