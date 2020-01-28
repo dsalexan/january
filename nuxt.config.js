f617e80a14692af979df685d2fecac1370c0d24d
dsalexan <danilo.salexan@gmail.com>
2020-01-28 17:02:23 -0300

    up command in package json

--- nuxt.config.js
+++ nuxt.config.js
@@ -61,7 +61,7 @@ export default {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.BASE_URL || 'http://167.172.246.25:3001'
   },
   auth: {
     redirect: {
