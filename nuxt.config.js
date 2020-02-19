034db967ab6d7a65ad6531089de2e6fc70662f83
dsalexan <danilo.salexan@gmail.com>
2020-02-19 20:55:08 -0300

    adding social tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,17 @@ export default {
       { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
       { rel: 'apple-touch-icon', type: 'image/x-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
+    ],
+    script: [
+      {
+        innerHTML: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '482133022613654');fbq('track', 'PageView');`
+      }
+    ],
+    noscript: [
+      {
+        innerHTML:
+          '<img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=482133022613654&ev=PageView&noscript=1"/>'
+      }
     ]
     /* <link rel="manifest" href="/site.webmanifest"> */
   },
